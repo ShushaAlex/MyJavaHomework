1fcb29c40406d18c936b326823c637d83da8aa58
Alex <alexandra.grigorchenko@gmail.com>
2023-09-01 11:25:14 +0200

    add homework day one

---
+++ src/main/java/de/telran/homework/one/TaskOne.java
@@ -0,0 +1,21 @@
+package de.telran.homework.one;
+
+public class TaskOne {
+    public static void main(String[] args) {
+        byte a1 = 4;
+        int a2 = 89;
+        short a3 = 56;
+        float a4 = 4.7333436f;
+        double a5 = 4.355453532;
+        long a6 = 12121;
+        char myLetter = 'G';
+
+        System.out.println("a1 is " + a1);
+        System.out.println("a2 is " + a2);
+        System.out.println("a3 is " + a3);
+        System.out.println("a4 is " + a4);
+        System.out.println("a5 is " + a5);
+        System.out.println("a6 is " + a6);
+        System.out.println("Letter is " + myLetter);
+    }
+}
---
+++ src/main/java/de/telran/homework/one/TaskTwo.java
@@ -0,0 +1,13 @@
+package de.telran.homework.one;
+
+public class TaskTwo {
+    public static void main(String[] args) {
+        int num = 345;
+        int firstChar = num / 115;
+        int secondChar = num - 341;
+        int thirdChar = num % 10;
+        System.out.print(firstChar + ", ");
+        System.out.print(secondChar + ", ");
+        System.out.print(thirdChar);
+    }
+}
