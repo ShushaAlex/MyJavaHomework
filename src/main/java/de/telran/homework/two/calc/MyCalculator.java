8a5d883c8a430595b8a825c4279c61f699254765
Alex <alexandra.grigorchenko@gmail.com>
2023-09-04 11:50:24 +0200

    init commit

--- src/main/java/de/telran/homework/two/calc/MyCalculator.java
+++ src/main/java/de/telran/homework/two/calc/MyCalculator.java
@@ -5,7 +5,7 @@ public class MyCalculator {
 
         Calculator myCalc = new Calculator();
 
-        System.out.println(myCalc.sum(10, 13));
+        System.out.println(myCalc.sum(10, 46));
         System.out.println(myCalc.sub(27, 6));
         System.out.println(myCalc.mult(9, 19));
         System.out.println(myCalc.div(76, 14));
