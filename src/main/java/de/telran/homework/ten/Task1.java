e764baf1dcfe1299e6b1321d9d55c67a3ac82096
Alex <alexandra.grigorchenko@gmail.com>
2023-09-22 12:07:18 +0200

    add homework 10

--- src/main/java/de/telran/homework/ten/Task1.java
+++ src/main/java/de/telran/homework/ten/Task1.java
@@ -1,16 +1,20 @@
 package de.telran.homework.ten;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 //Напишите программу, которая меняет местами элементы одномерного массива из String в обратном порядке.
 //Не используйте дополнительный массив для хранения результатов.
 
 public class Task1 {
     public static void main(String[] args) {
-        String[] myArr = {"aaa","bbb", "ccc", "ddd", "eee"};
-        System.out.println(Arrays.toString(myArr));
-        reverseStrArr(myArr);
-        System.out.println(Arrays.toString(myArr));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your string: ");
+        String inputString = scanner.nextLine();
+        String[] stringArr = inputString.split(" ");
+        System.out.println(Arrays.toString(stringArr));
+        reverseStrArr(stringArr);
+        System.out.println(Arrays.toString(stringArr));
     }
     public static void reverseStrArr(String[] arr) {
         int lastChar = arr.length - 1;
